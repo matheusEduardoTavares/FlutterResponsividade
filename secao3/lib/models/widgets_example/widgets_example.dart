280f54aa4b4be820c46c2973f60aaa6d7c565cbf
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-07-09 22:36:48 -0300

    Aula 14 - Constraints na Pratica - Parte 3 - finalizada

---
+++ secao3/lib/models/widgets_example/widgets_example.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class WidgetsExample {
+  const WidgetsExample({
+    @required this.title,
+    @required this.child,
+  });
+
+  final String title;
+  final Widget child;
+}
--- secao3/lib/utils/pages_definition.dart
+++ secao3/lib/utils/pages_definition.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:secao3/models/pages_definition/pages_definition_model.dart';
 import 'package:secao3/pages/constraints_page/constraints_page.dart';
 import 'package:secao3/pages/home_page/home_page.dart';
+import 'package:secao3/pages/texts_pages/texts_pages.dart';
 
 abstract class PagesDefinition {
   static const homePage = '/home';
   static const constraintsPage = '/constraints-pages';
+  static const TextsPage = '/texts-pages';
 
   static final pagesName = <String, Widget Function(BuildContext)>{
     homePage: (BuildContext _) => HomePage(),
-    constraintsPage: (BuildContext _) => ConstraintsPage()
+    constraintsPage: (BuildContext _) => ConstraintsPage(),
+    TextsPage: (BuildContext _) => TextsPages(),
   };
 
   static final pages = <PagesDefinitionModel>[
@@ -22,6 +25,11 @@ abstract class PagesDefinition {
       leading: const Icon(Icons.two_k),
       title: 'Constraints',
       route: constraintsPage,
+    ),
+    PagesDefinitionModel(
+      leading: const Icon(Icons.three_k),
+      title: 'Texts',
+      route: TextsPage,
     ),
   ];
 }
