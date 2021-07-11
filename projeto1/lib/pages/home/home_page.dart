b949c5baaabfd4bd045f172b146e52ae4d172967
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-07-11 14:23:24 -0300

    Aula 22 - Explorando o LayoutBuilder na Pratica - finalizado

---
+++ projeto1/lib/pages/home/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    /*
+    return LayoutBuilder(
+      builder: (_, constraints) {
+        ///O mesmo que printar constraints.maxHeight e constraints.maxWidth
+        print('biggest ${constraints.biggest}');
+        ///O mesmo que printar constraints.minHeight e constraints.minWidth
+        print('smallest ${constraints.smallest}');
+        ///Os tamanhos printados ficarão todos grandes 
+        ///pois já vimos que o primeiro Widget recebe 
+        ///constraints do tipo tight para ocupar todo 
+        ///o espaço da tela, então o constraints.biggest 
+        ///acabará sendo igual ao constraints.smallest
+        return Container();
+      },
+    );
+    */
+
+    /*
+    ///Agora é o [Center] quem recebe a constraint tight,
+    ///portanto o [LayoutBuilder] irá começar a mostrar 
+    ///o [constraints.biggest] como o tamanho da tela e o
+    ///[constraints.smallest] como (0.0, 0.0)
+    return Center(
+      child: LayoutBuilder(
+        builder: (_, constraints) {
+          print('biggest ${constraints.biggest}');
+          print('smallest ${constraints.smallest}');
+          return Container();
+        },
+      ),
+    );
+    */
+
+    return LayoutBuilder(
+      builder: (_, constraints) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: constraints.maxWidth >= 800 ? 
+            Colors.red : Colors.blue,
+        ),
+      ),
+    );
+  }
+}
