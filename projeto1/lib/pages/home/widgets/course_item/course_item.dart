9643523ef1a897d866894cf75b11af0af810da5d
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-07-17 21:41:01 -0300

    Aula 33 - Utilizando o AutoSizeText - finalizada

--- projeto1/lib/pages/home/widgets/course_item/course_item.dart
+++ projeto1/lib/pages/home/widgets/course_item/course_item.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class CourseItem extends StatelessWidget {
@@ -13,14 +14,32 @@ class CourseItem extends StatelessWidget {
           fit: BoxFit.fitWidth,
         ),
         const SizedBox(height: 4.0,),
-        Text(
-          'Criação de Apps Android e iOS com Flutter - Crie 16 Apps',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 15.0,
-            color: Colors.white,
+        Flexible(
+          child: LayoutBuilder(
+            builder: (_, constraints) {
+              print('constraints.biggest = ${constraints.biggest}');
+              print('constraints.smallest = ${constraints.smallest}');
+
+              return AutoSizeText(
+                'Criação de Apps Android e iOS com Flutter - Crie 16 Apps',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15.0,
+                  color: Colors.white,
+                ),
+              );
+            },
           ),
         ),
+        // Text(
+        //   'Criação de Apps Android e iOS com Flutter - Crie 16 Apps',
+        //   maxLines: 2,
+        //   style: TextStyle(
+        //     fontWeight: FontWeight.bold,
+        //     fontSize: 15.0,
+        //     color: Colors.white,
+        //   ),
+        // ),
         Text(
           'Daniel Ciolfi',
           style: TextStyle(
