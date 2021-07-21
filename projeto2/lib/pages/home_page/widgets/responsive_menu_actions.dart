6f2d6cee8e6a5544b74ba3e248ec2c90419929f6
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-07-21 20:56:44 -0300

    Aula 40 - Finalizando a AppBar Responsiva - finalizada

---
+++ projeto2/lib/pages/home_page/widgets/responsive_menu_actions.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_value.dart';
+import 'package:responsive_framework/responsive_wrapper.dart';
+
+class ResponsiveMenuActions extends StatelessWidget {
+  const ResponsiveMenuActions({ 
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        ResponsiveVisibility(
+          visible: false,
+          visibleWhen: [
+            Condition.smallerThan(name: TABLET)
+          ],
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+        ),
+        const SizedBox(width: 4,),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.home),
+        ),
+        const SizedBox(width: 4,),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.send),
+        ),
+        const SizedBox(width: 4,),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.favorite_border),
+        ),
+        const SizedBox(width: 16,),
+        CircleAvatar(
+          radius: 16,
+          backgroundImage: NetworkImage(
+            'https://blog.logrocket.com/wp-content/uploads/2021/05/intro-dart-flutter-feature.png',
+          ),
+        ),
+      ],
+    );
+  }
+}
