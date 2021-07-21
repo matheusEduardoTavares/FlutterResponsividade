45872c4c94e6b3099a85ba4e8bfe2eae8330d85a
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2021-07-20 21:51:08 -0300

    Aula 38 - Criando o Projeto - finalizada

--- projeto2/lib/main.dart
+++ projeto2/lib/main.dart
@@ -1,113 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:projeto2/pages/home_page/home_page.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Exemplo 2',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      builder: (context, widget) => ResponsiveWrapper.builder(
+        ClampingScrollWrapper.builder(context, widget),
+        ///Aqui adicionamos configurações para o [ResponsiveWrapper]
+        ///Só de trocar o [defaultScale] para true é mudado o 
+        ///comportamento dos widgets na tela, pois o comportamento
+        ///padrão do flutter é só reajustar (resize) os componentes
+        ///na tela, mas com o [defaultScale] true, o comportamento
+        ///é mudado para dar zoom in ou zoom out nos componentes,
+        ///de forma que o tamanho deles irá mudar devido a isso.
+        ///O Widget em si continua tendo o seu tamanho normal, 
+        ///mas o [ResponsiveFramework] faz esse ajuste de escala
+        ///que dá a impressão de mudar o tamanho do widget. Mas
+        ///manter o mesmo comportamento de escala para qualquer
+        ///tamanho de tela não fica legal, por isso é importante
+        ///explorar outros atributos do [ResponsiveWrapper.builder]
+        ///como os [breakpoints], o [minWidth], [maxWidth], entre outros
+        defaultScale: true,
+        minWidth: 450,
+        breakpoints: [
+          // ResponsiveBreakpoint.autoScale(
+          ResponsiveBreakpoint.resize(450, name: MOBILE,),
+          ResponsiveBreakpoint.resize(700, name: TABLET,),
+          ResponsiveBreakpoint.resize(800, name: DESKTOP,),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: HomePage(),
     );
   }
 }
